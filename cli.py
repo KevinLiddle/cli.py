f6de5de4722e2241e6f8e8056fa9353e2aaf64e9
Kevin Liddle <kevin@8thlight.com>
2017-05-19 16:02:50 -0500

    For your CLIs

---
+++ cli.py
@@ -0,0 +1,36 @@
+print """
+                                            `-:/+o++/-`
+                                          `-/++////+oso/`           ___________________________
+                                         .-+/`       .oy+.        /                             \\
+                                        .-+:          `ys/       |    It looks like you're       |
+                                   `:+yhdmmho`         os+`      |    trying to run a CLI...     |
+                                 .+so/-:+y+os.        `yo+`       \\_     _______________________/
+                                `:.````-//            /mmmdy+-      /  _/
+                                ````````.-..          /+s::+shy-   /_/
+                               `  `/oyyy+.`-:`       `--:`````:y:
+                              ..  sddmNNNd..::    `   `..``.--``/`
+                              `-``/dNNNMNy.-/:   `  `+yhddy:`.:.
+                               `----:+++:-:/:`  `-` :NNNNNMm..:/
+                                 `.-::oo+:-`     --.`+hmmmd+.-/:
+                                     `hdo         .-::-::::://-`
+                                     `yho           .hhs----.`
+                                     `hyo /o:       .ddy    -/-
+                                     `hyo sho       .ddo   :yh:
+                                     `hyo oy+       .dh/  .sho
+                                     `yso oy+       .hy/  /sh-
+                                      sso`+s+       `hy: `ooy`
+                                      ss+`/s+       `yy: `soo
+                                      +y/.:s+`       sy/ `soo
+                                      :y/-.y+-       +s+  ooo`
+                                      .h/: os:`      /o/  +oo`
+                                       yo:`.so:`    .:+-  /so-
+                                       /y:. .os+:--:/+-   :ys:
+                                       .h+:   -/+++/:`    :hs/
+                                        +y:.              :hs:
+                                        `y+-`             :ss.
+                                         :y:-            `/o+`
+                                          /y/-`         `-/+.
+                                           :yo/-`    `..-:/.
+                                            `+ys+/:::////-
+                                               .:::::-.`
+"""
